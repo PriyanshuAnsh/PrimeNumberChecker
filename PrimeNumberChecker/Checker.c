fd0402cf7e7b26f3cacccf2670bf79dc09794e99
Priyanshu Dongre <153956062+PriyanshuAnsh@users.noreply.github.com>
2024-12-13 16:12:02 -0600

    Add files via upload

---
+++ PrimeNumberChecker/Checker.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int isPrime(int n){
+
+    if (n <= 0) {
+        return 0;
+    }
+    for(int i = 2; i <= n && i != n; i++) {
+        if(n % i == 0) {
+            return 0;
+        }
+    }
+
+    return 1;
+}
+
+int main() {
+    int number;
+    printf("Enter a number: ");
+    scanf("%d", &number);
+
+    if(isPrime(number) == 1) {
+        printf("%d is a prime number\n", number);
+    } else {
+        printf("%d is not a prime number\n", number);
+    }
+    return 0;
+
+
+}
